54a66a054da12f8b5643470d6bcc83bc92eb09c9
Naga Rohitha Nannapaneni <nagarohitha@usf.edu>
2019-08-29 19:31:28 -0400

    Add project files.

---
+++ ISM3232A3NannapaneniRohitha/frmISM3232A3ByRohithaNannapaneni.Designer.vb
@@ -0,0 +1,94 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class frmISM3232A3ByRohithaNannapaneni
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()> _
+    Private Sub InitializeComponent()
+        Me.lblTitle = New System.Windows.Forms.Label()
+        Me.btnArray = New System.Windows.Forms.Button()
+        Me.btnSelect = New System.Windows.Forms.Button()
+        Me.btnIF = New System.Windows.Forms.Button()
+        Me.SuspendLayout()
+        '
+        'lblTitle
+        '
+        Me.lblTitle.AutoSize = True
+        Me.lblTitle.Font = New System.Drawing.Font("Microsoft Sans Serif", 14.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.lblTitle.Location = New System.Drawing.Point(99, 63)
+        Me.lblTitle.Name = "lblTitle"
+        Me.lblTitle.Size = New System.Drawing.Size(425, 24)
+        Me.lblTitle.TabIndex = 0
+        Me.lblTitle.Text = "ISM 3232, Assignment 3 By Rohitha Nannapaneni"
+        '
+        'btnArray
+        '
+        Me.btnArray.BackColor = System.Drawing.Color.Coral
+        Me.btnArray.Font = New System.Drawing.Font("Microsoft Sans Serif", 12.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.btnArray.Location = New System.Drawing.Point(224, 126)
+        Me.btnArray.Name = "btnArray"
+        Me.btnArray.Size = New System.Drawing.Size(211, 51)
+        Me.btnArray.TabIndex = 1
+        Me.btnArray.Text = "Prime Number Array"
+        Me.btnArray.UseVisualStyleBackColor = False
+        '
+        'btnSelect
+        '
+        Me.btnSelect.BackColor = System.Drawing.Color.Plum
+        Me.btnSelect.Font = New System.Drawing.Font("Microsoft Sans Serif", 12.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.btnSelect.Location = New System.Drawing.Point(367, 253)
+        Me.btnSelect.Name = "btnSelect"
+        Me.btnSelect.Size = New System.Drawing.Size(188, 81)
+        Me.btnSelect.TabIndex = 2
+        Me.btnSelect.Text = "BMI and Select Case Statement"
+        Me.btnSelect.UseVisualStyleBackColor = False
+        '
+        'btnIF
+        '
+        Me.btnIF.BackColor = System.Drawing.Color.LightSteelBlue
+        Me.btnIF.Font = New System.Drawing.Font("Microsoft Sans Serif", 12.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.btnIF.Location = New System.Drawing.Point(103, 253)
+        Me.btnIF.Name = "btnIF"
+        Me.btnIF.Size = New System.Drawing.Size(188, 81)
+        Me.btnIF.TabIndex = 3
+        Me.btnIF.Text = "BMI and IF Statement"
+        Me.btnIF.UseVisualStyleBackColor = False
+        '
+        'frmISM3232A3ByRohithaNannapaneni
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.ClientSize = New System.Drawing.Size(638, 484)
+        Me.Controls.Add(Me.btnIF)
+        Me.Controls.Add(Me.btnSelect)
+        Me.Controls.Add(Me.btnArray)
+        Me.Controls.Add(Me.lblTitle)
+        Me.Name = "frmISM3232A3ByRohithaNannapaneni"
+        Me.Text = "ISM 3232 Assignment 3 By Rohitha Nannapaneni"
+        Me.ResumeLayout(False)
+        Me.PerformLayout()
+
+    End Sub
+
+    Friend WithEvents lblTitle As Label
+    Friend WithEvents btnArray As Button
+    Friend WithEvents btnSelect As Button
+    Friend WithEvents btnIF As Button
+End Class
---
+++ ISM3232A3NannapaneniRohitha/frmISM3232A3ByRohithaNannapaneni.vb
@@ -0,0 +1,70 @@
+﻿Public Class frmISM3232A3ByRohithaNannapaneni
+    Private Sub btnArray_Click(sender As Object, e As EventArgs) Handles btnArray.Click
+        Dim PrimeNumber(6) As Single
+        PrimeNumber(1) = 1
+        PrimeNumber(2) = 2
+        PrimeNumber(3) = 3
+        PrimeNumber(4) = 5
+        PrimeNumber(5) = 7
+        PrimeNumber(6) = 11
+        Dim i = 0
+        'LOOP Statements
+        While i < 6
+            i = i + 1
+            MsgBox(PrimeNumber(i))
+        End While
+
+        ReDim Preserve PrimeNumber(8)
+        PrimeNumber(7) = 13
+        PrimeNumber(8) = 17
+        'LOOP Statements
+        Do While i < 8
+            i = i + 1
+            MsgBox(PrimeNumber(i))
+        Loop
+    End Sub
+
+    Private Sub btnIF_Click(sender As Object, e As EventArgs) Handles btnIF.Click
+        Dim Weight As Single
+        Weight = InputBox("Please enter your weight in pounds(lbs)")
+        Dim Height As Single
+        Height = InputBox("Please enter your height in inches(in)")
+        Dim BMI As Short
+        BMI = Weight * 703 / (Height ^ 2)
+        MsgBox("Your Body Mass Index is " & BMI)
+
+        'IF-THEN-ELSE Statements
+        If BMI < 18.5 Then
+            MsgBox("You're underweight.")
+        ElseIf BMI < 24.9999 Then
+            MsgBox("You're normal weight.")
+        ElseIf BMI < 29.999 Then
+            MsgBox("You're overweight.")
+        ElseIf BMI > 30 Then
+            MsgBox("You're obese.")
+        End If
+
+    End Sub
+
+    Private Sub btnSelect_Click(sender As Object, e As EventArgs) Handles btnSelect.Click
+        Dim Weight As Single
+        Weight = InputBox("Please enter your weight in pounds(lbs)")
+        Dim Height As Single
+        Height = InputBox("Please enter your height in inches(in)")
+        Dim BMI As Short
+        BMI = Weight * 703 / (Height ^ 2)
+        MsgBox("Your Body Mass Index is " & BMI)
+
+        'SELECT-CASE Statements
+        Select Case BMI
+            Case Is < 18.5
+                MsgBox("You're underweight.")
+            Case Is < 24.9999
+                MsgBox("You're normal weight.")
+            Case Is < 29.9999
+                MsgBox("You're overweight.")
+            Case Is >= 30
+                MsgBox("You're obese")
+        End Select
+    End Sub
+End Class
